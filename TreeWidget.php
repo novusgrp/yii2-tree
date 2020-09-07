8f606fb6b2606b758bf452ce5c72dd6016c449e3
Armand <you@example.com>
2020-09-07 06:56:31 +0200

    updates

--- TreeWidget.php
+++ TreeWidget.php
@@ -17,7 +17,8 @@ use yii\helpers\Url;
  *
  * @author Armand Groenewald <armand@novusgroup.co.za>
  */
-class TreeWidget extends \yii\base\Widget {
+class TreeWidget extends \yii\base\Widget
+{
 
     /**
      * @var array
@@ -35,7 +36,8 @@ class TreeWidget extends \yii\base\Widget {
     /**
      * @inheritdoc
      */
-    public function init() {
+    public function init()
+    {
         parent::init();
         $this->registerAssets();
     }
@@ -43,7 +45,8 @@ class TreeWidget extends \yii\base\Widget {
     /**
      * Registers the needed assets
      */
-    public function registerAssets() {
+    public function registerAssets()
+    {
         $this->modelclass = $this->modelclass;
 
         $source = empty($this->model) ? [] : $this->buildSource($this->model);
@@ -63,6 +66,7 @@ class TreeWidget extends \yii\base\Widget {
         } else {
             echo Html::tag('div', '', ['id' => $id]);
         }
+        echo "<style>span.fancytree-title {width: 90%;}</style>";
         $options = Json::encode($this->options);
 
         if ($this->editable) {
@@ -73,8 +77,6 @@ class TreeWidget extends \yii\base\Widget {
         $this->options['expanded'] = true;
 
         $options = Json::encode($this->options);
-
-
 
 
         $view->registerJs('$("#' . $id . '").fancytree( ' . $options . ')');
@@ -94,18 +96,21 @@ class TreeWidget extends \yii\base\Widget {
           }');
     }
 
-    private function buildSource($model) {
+
+    private function buildSource($model)
+    {
         foreach ($model as $item) {
             $items[] = $item;
         }
 
         $model = $items;
         $source = [];
+
         foreach ($model as $item) {
             $data = $this->buildAttributes($item);
 
             $data['key'] = $item->{$this->keyfield};
-            $data['title'] = $item->{$this->titlefield};
+            $data['title'] = $item->{$this->titlefield} . $this->buildSources($item->{$this->keyfield});
             $data['expanded'] = true;
             $data['folder'] = (($item->{$this->typefield} == 1) ? true : false);
             $querymodel = $this->modelclass . 'Query';
@@ -120,12 +125,41 @@ class TreeWidget extends \yii\base\Widget {
         return $source;
     }
 
-    private function buildAttributes($object) {
+    private function buildAttributes($object)
+    {
         $attributes = $object->attributes();
         foreach ($attributes as $attribute) {
             $data[$attribute] = $object->{$attribute};
         }
         return $data;
+    }
+
+    private function buildSources($id)
+    {
+        $querymodel = $this->modelclass . 'Query';
+        $query = new $querymodel($this->modelclass);
+        $category = $query->where(['id' => $id])->One();
+        $message = '<span style="float:right; margin-right: 50px;"> &nbsp;';
+        if ($category->is_online + $category->is_online_headlines > 0) {
+            $message .= '<i class="fa fa-globe" aria-hidden="true">&nbsp;</i>';
+        }
+        if ($category->is_print + $category->is_print_headlines > 0) {
+            $message .= '<i class="fas fa-newspaper" aria-hidden="true">&nbsp;</i>';
+        }
+        if ($category->is_broadcast > 0) {
+            $message .= '<i class="fas fa-microphone-alt" aria-hidden="true">&nbsp;</i>';
+        }
+        if ($category->is_twitter > 0) {
+            $message .= '<i class="fab fa-twitter" aria-hidden="true">&nbsp;</i>';
+        }
+        if ($category->is_facebook > 0) {
+            $message .= '<i class="fab fa-facebook-f" aria-hidden="true">&nbsp;</i>';
+        }
+        if ($category->is_instagram > 0) {
+            $message .= '<i class="fab fa-instagram" aria-hidden="true">&nbsp;</i>';
+        }
+        $message .= '</span>';
+        return $message;
     }
 
 }
